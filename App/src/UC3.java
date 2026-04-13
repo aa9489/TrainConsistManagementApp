748d00fb1f2360d72d044ff102e2171a2b39c7e0
aa9489 <aa9489@srmist.edu.in>
2026-04-13 11:21:18 +0530

    aa9489 added uc3

---
+++ App/src/UC3.java
@@ -0,0 +1,16 @@
+import java.util.HashSet;
+
+public class UC3 {
+    public static void main(String[] args) {
+        HashSet<String> bogieIds = new HashSet<>();
+
+        bogieIds.add("BG101");
+        bogieIds.add("BG102");
+        bogieIds.add("BG103");
+        bogieIds.add("BG101");
+        bogieIds.add("BG102");
+
+        System.out.println("Unique Bogie IDs:");
+        System.out.println(bogieIds);
+    }
+}
