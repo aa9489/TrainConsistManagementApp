50010825bcff0c5a93b5dffc1f6713c5fe0f1bef
aa9489 <aa9489@srmist.edu.in>
2026-04-29 09:11:04 +0530

    aa9489 added uc5;

---
+++ App/src/uc5.java
@@ -0,0 +1,15 @@
+import java.util.LinkedHashSet;
+
+public class uc5 {
+    public static void main(String[] args) {
+        LinkedHashSet<String> formation = new LinkedHashSet<>();
+
+        formation.add("Engine");
+        formation.add("Sleeper");
+        formation.add("Cargo");
+        formation.add("Guard");
+        formation.add("Sleeper");
+
+        System.out.println(formation);
+    }
+}
